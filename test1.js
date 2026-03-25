504fec4eca263d60ef00b62415f79a898bc55cd0
gouse-work <msk.work91@gmail.com>
2026-03-25 17:54:13 +0530

    method-8

--- test1.js
+++ test1.js
@@ -2,4 +2,9 @@ console.log("output printed");
 console.log("Ai Hacathon");
 console.log("method-2");
 console.log("method-03");
-consol.log("Ai Haca")
+let age = 10;
+if (age > 18) { // Logical error: condition is wrong if checking for a child
+  console.log("Adult");
+} else {
+  console.log("Child");
+}
